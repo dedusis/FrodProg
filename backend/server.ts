81fb67c6d5feed6597adabc532a176fd8ddb830c
dedusis <leodedusis@gmail.com>
2026-03-25 19:11:47 +0200

    added swagger.yaml

--- backend/server.ts
+++ backend/server.ts
@@ -2,6 +2,8 @@ import express from 'express';
 import cors from 'cors';
 import dotenv from 'dotenv';
 import router from './src/routes/router';
+import swaggerUi from 'swagger-ui-express';
+import YAML from 'yamljs';
 import { connectDB } from './src/config/db';
 
 
@@ -14,13 +16,17 @@ app.use(express.json());
 
 app.use('/api', router);
 
+const swaggerDocument = YAML.load('./swagger.yaml');
+
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
+
 const PORT = process.env.PORT || 3000;
 
 const startServer = async () => {
     await connectDB();
 
     app.listen(PORT, () => {
-        console.log(`Server is running on port ${PORT}`);
+        console.log(`Server is running on http://localhost:${PORT}`);
     });
 };
 
